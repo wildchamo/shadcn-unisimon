26263baac2a6c121d90e48ec3a90df327934ead2
Jose Luis <wildchamo@gmail.com>
2025-09-23 18:19:05 -0500

    feat: add success variant to Button component and showcase variants on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,20 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <main>
       <h1>Home</h1>
+      <div className="flex gap-2">
+        <Button variant="default" size="lg">
+          Button
+        </Button>
+        <Button variant="destructive">Button</Button>
+        <Button variant="outline">Button</Button>
+        <Button variant="secondary">Button</Button>
+        <Button variant="ghost">Button</Button>
+        <Button variant="link">Button</Button>
+        <Button variant="success">Success</Button>
+      </div>
     </main>
   );
 }
